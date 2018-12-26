5de0380310050baf31392e2fc51a3b6d2e3c48e0
Адылканов Муратжан Талгатович <murka_505853@mail.ru>
2018-12-27 04:01:47 +0600

    Сделали домашнюю работу #1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Написать приложение, которое выведет на экран Ваше имя и фамилию.
+            //var imya = "Муратжан";
+            //var familiya = "Адылканов";
+            //System.Console.WriteLine("Имя: " + imya);
+            //System.Console.WriteLine("Фамилия: " + familiya);
+            //System.Console.ReadLine();
+            
+
+            //2.Написать приложение, которое ожидает ввода нескольких чисел и выводит на экран их сумму.
+            //System.Console.WriteLine("Введите несколько чисел: ");
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Сумма этих чисел: " + (a + b + c));
+            //Console.ReadLine();
+        }
+    }
+}
